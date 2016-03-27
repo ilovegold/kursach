52f23d403584a3148a897fe82d15dd42dc1c325f
ilovegold <ilovegoldandyou@gmail.com>
2016-03-28 00:49:27 +0300

    Finish layout and routes

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module App2
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *svg)
   end
 end
